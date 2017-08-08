9d86dd4cb266960f0268e1addecd0fd4a9ee9daf
Kevin <dackowkevin@gmail.com>
2017-08-07 21:05:35 -0400

    Modified news to allow for resizing of twitter feeds depending on number of selected twitter feeds

--- news.js
+++ news.js
@@ -1,7 +1,15 @@
-function showMe(checkbox, newsfeed) {
+function showMe(checkbox, twitterfeed) {
+    var chboxs = document.getElementsByName("newsSource");
+    var columns = 0;
+    for(i=0;i<chboxs.length;i++){
+        if(chboxs[i].checked){
+            columns = columns + 1;
+        }
+    }
+    document.getElementById("newsfeed").getElementsByTagName("ul")[0].style.columns = columns;
     var vis = "none";
     if(checkbox.checked){
         vis = "block";
     }
-    document.getElementById(newsfeed).style.display = vis;
+    document.getElementById(twitterfeed).style.display = vis;
 }
