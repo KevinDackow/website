2660c45f321817abe1967c47088d51c8db7de9a4
Kevin <dackowkevin@gmail.com>
2017-08-07 21:30:15 -0400

    add show and hide all functionality

--- news.js
+++ news.js
@@ -1,4 +1,15 @@
-function showMe(checkbox, twitterfeed) {
+// Determines if feed should be hidden or unhidden and does it
+function feedChecker(checkbox, twitterfeed) {
+    if(checkbox.checked){
+        document.getElementById(twitterfeed).style.display = "block";
+    } else {
+        document.getElementById(twitterfeed).style.display = "none";
+    }
+    modifyFeedSizes();
+}
+
+// Modifies the sizes of twitter feeds based on number of open feeds
+function modifyFeedSizes() {
     var chboxs = document.getElementsByName("newsSource");
     var columns = 0;
     for(i=0;i<chboxs.length;i++){
@@ -7,9 +18,31 @@ function showMe(checkbox, twitterfeed) {
         }
     }
     document.getElementById("newsfeed").getElementsByTagName("ul")[0].style.columns = columns;
-    var vis = "none";
-    if(checkbox.checked){
-        vis = "block";
+}
+
+
+//Shows all feeds
+function showAll() {
+    var chboxs = document.getElementsByName("newsSource");
+    for(i=0;i<chboxs.length;i++){
+        chboxs[i].checked = true;
     }
-    document.getElementById(twitterfeed).style.display = vis;
+    var tweets = document.getElementsByClassName("twitterfeed");
+    for(i=0;i<tweets.length;i++){
+        tweets[i].style.display = "block";
+    }
+    modifyFeedSizes();
+}
+
+//Hides all feeds
+function hideAll() {
+    var chboxs = document.getElementsByName("newsSource");
+    for(i=0;i<chboxs.length;i++){
+        chboxs[i].checked = false;
+    }
+    var tweets = document.getElementsByClassName("twitterfeed");
+    for(i=0;i<tweets.length;i++){
+        tweets[i].style.display = "none";
+    }
+    modifyFeedSizes();
 }
