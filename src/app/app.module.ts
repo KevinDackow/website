72da0d0340823a683b016446fb1fe2153e0837ec
Kevin Dackow <kjdackow@gmail.com>
2020-02-09 22:26:07 -0800

    Deployable site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ParticlesModule } from 'angular-particle';
 import { HeroComponent } from './hero/hero.component';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
@@ -24,7 +23,6 @@ import { ResumeComponent } from './resume/resume.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
